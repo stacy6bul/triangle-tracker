b939ea959745cec48e394a6b4bd0de9c9158ed86
stacy6bul <adhissatc@gmail.com>
2019-02-18 16:55:02 -0500

    added alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 var triangle = function(sideA, sideB, sideC) {
   if ( Triangle(sideA, sideB, sideC));
-   return triangleType(sideA, sideB, sideC)
+   return triangleType(sideA, sideB, sideC);
 } else {
   return "Not a triangle.";
+  alert ("Not a triangle");
 };
+
+var triangleType = function(sideA ,sideB, sideC) {
+  if (sideA===sideB===sideC)
+}
